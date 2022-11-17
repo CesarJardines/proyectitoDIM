0212aff37df0f86e83198a87aff5a30376a2b002
César Jardines <cesarjardines@MacBook-Pro-de-Cesar.local>
2022-11-17 09:41:32 -0600

    Se agrega código de la clase del 17/11/22

--- proyectito/miPrimerApp/views.py
+++ proyectito/miPrimerApp/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.decorators import login_required
 #se agregan estas linas
 from .form import *
 from django.contrib.auth import authenticate, login
+
+from datetime import datetime
+
 #Create your views here.
 @login_required
 def index(request):
@@ -18,12 +21,21 @@ def index(request):
     print(consulta_Estudiante)
     grupo_idVariable = Grupo.objects.get(id_grupo=1)
     grupo_idVariableDos = Grupo.objects.get(id_grupo=2)
+    hoy = datetime.now()
+    print(hoy.weekday())
     return render(request,'index.html', {'consulta_Estudiante':consulta_Estudiante, 'grupo_idVariable':grupo_idVariable, 'grupo_idVariableDos':grupo_idVariableDos})
 @login_required
 def funcionUno(request, grupo_id):
     #Obtenemos aquellos estudiantes que están en el grupo con id cinco
-    consulta_Estudiante  =  Estudiante.objects.filter(grupo=1)
+    consulta_Estudiante  =  Estudiante.objects.filter(grupo=2)
+    grupo_idVariable = Grupo.objects.get(id_grupo=1)
     print(consulta_Estudiante)
+    hoy = datetime.now()
+    print(hoy.weekday())
+
+    if hoy.weekday() == 3: 
+        #return HttpResponse('Hoy es jueves') 
+        return redirect('miPrimerApp:DOS', grupo_id=grupo_idVariable) 
     return render(request,'Grupo1.html', {'consulta_Estudiante':consulta_Estudiante, 'grupo_id':grupo_id})
 @login_required
 def funcionDos(request, grupo_id):
